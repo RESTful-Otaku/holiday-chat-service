d6be763fc0bf37f84df7e00d90bd4b95e474d48a
RESTful_Otaku <jonno@jmh-deb.jmh-deb>
2023-06-22 14:11:38 +0100

    Update filter to support partial matches

--- src/filter.rs
+++ src/filter.rs
@@ -18,9 +18,16 @@ impl Filter {
     pub fn matches(&self, hotel: &super::models::Hotel) -> bool {
         for (key, value) in &self.criteria {
             match key.as_str() {
-                "city" => if hotel.city != *value { return false; },
-                "star_rating" => if hotel.star_rating.to_string() != *value { return false; },
-                "temp_rating" => if hotel.temp_rating != *value { return false; },
+                "holiday_reference" => if !hotel.holiday_reference.to_string().contains(value) { return false; },
+                "hotel_name" => if !hotel.hotel_name.contains(value) { return false; },
+                "city" => if !hotel.city.contains(value) { return false; },
+                "continent" => if !hotel.continent.contains(value) { return false; },
+                "country" => if !hotel.country.contains(value) { return false; },
+                "category" => if !hotel.category.contains(value) { return false; },
+                "star_rating" => if !hotel.star_rating.to_string().contains(value) { return false; },
+                "temp_rating" => if !hotel.temp_rating.contains(value) { return false; },
+                "location" => if !hotel.location.contains(value) { return false; },
+                "price_per_night" => if !hotel.price_per_night.to_string().contains(value) { return false; },
                 _ => {},
             }
         }

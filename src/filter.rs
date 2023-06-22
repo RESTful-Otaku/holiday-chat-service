5ca7a531a8324d2abe096ff09cd4571c4cfbe780
RESTful_Otaku <jonno@jmh-deb.jmh-deb>
2023-06-22 14:12:29 +0100

    Add case sensitive support for matching

--- src/filter.rs
+++ src/filter.rs
@@ -16,22 +16,23 @@ impl Filter {
     }
 
     pub fn matches(&self, hotel: &super::models::Hotel) -> bool {
-        for (key, value) in &self.criteria {
-            match key.as_str() {
-                "holiday_reference" => if !hotel.holiday_reference.to_string().contains(value) { return false; },
-                "hotel_name" => if !hotel.hotel_name.contains(value) { return false; },
-                "city" => if !hotel.city.contains(value) { return false; },
-                "continent" => if !hotel.continent.contains(value) { return false; },
-                "country" => if !hotel.country.contains(value) { return false; },
-                "category" => if !hotel.category.contains(value) { return false; },
-                "star_rating" => if !hotel.star_rating.to_string().contains(value) { return false; },
-                "temp_rating" => if !hotel.temp_rating.contains(value) { return false; },
-                "location" => if !hotel.location.contains(value) { return false; },
-                "price_per_night" => if !hotel.price_per_night.to_string().contains(value) { return false; },
-                _ => {},
-            }
-        }
-
-        true
-    }
+      for (key, value) in &self.criteria {
+          let value = value.to_lowercase();
+          match key.as_str() {
+              "holiday_reference" => if !hotel.holiday_reference.to_string().to_lowercase().contains(&value) { return false; },
+              "hotel_name" => if !hotel.hotel_name.to_lowercase().contains(&value) { return false; },
+              "city" => if !hotel.city.to_lowercase().contains(&value) { return false; },
+              "continent" => if !hotel.continent.to_lowercase().contains(&value) { return false; },
+              "country" => if !hotel.country.to_lowercase().contains(&value) { return false; },
+              "category" => if !hotel.category.to_lowercase().contains(&value) { return false; },
+              "star_rating" => if !hotel.star_rating.to_string().to_lowercase().contains(&value) { return false; },
+              "temp_rating" => if !hotel.temp_rating.to_lowercase().contains(&value) { return false; },
+              "location" => if !hotel.location.to_lowercase().contains(&value) { return false; },
+              "price_per_night" => if !hotel.price_per_night.to_string().to_lowercase().contains(&value) { return false; },
+              _ => {},
+          }
+      }
+  
+      true
+  }  
 }

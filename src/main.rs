15917bfbfb0c1decd187c49beb4e61aa256f6db4
RESTful_Otaku <jonno@jmh-deb.jmh-deb>
2023-06-22 14:15:50 +0100

    Set up HTTP server and routes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,39 @@
-mod models;
-
-#![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use] extern crate rocket;
 
-fn main() {
-    rocket::ignite().launch();
+use rocket::State;
+use rocket::response::content;
+use rocket::serde::json::Json;
+use rocket_cors::{AllowedOrigins, CorsOptions};
+
+mod models;
+mod filter;
+
+use models::Hotel;
+use filter::Filter;
+
+struct AppState {
+    hotels: Vec<Hotel>,
+}
+
+#[derive(Deserialize)]
+struct Message {
+    message: String,
+}
+
+#[post("/message", format = "json", data = "<message>")]
+fn message(message: Json<Message>, state: State<AppState>) -> content::Plain<String> {
+    // ... your message handling logic here ...
+}
+
+#[launch]
+fn rocket() -> _ {
+    let cors = CorsOptions::default()
+        .allowed_origins(AllowedOrigins::all())
+        .allow_credentials(true)
+        .to_cors().unwrap();
+
+    rocket::build()
+        .mount("/", routes![message])
+        .attach(cors)
+        .manage(AppState { hotels: read_hotels().unwrap() })
 }
